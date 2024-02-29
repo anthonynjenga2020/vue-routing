9717d9e8dcd092a954dd7b1c5955f50088c9fff2
anthonynjenga254 <anthonynjenga2020@gmail.com>
2024-02-29 21:57:50 +0300

    practiced key concepts in vue routing, like dynamic and programmatic routing

---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="container">
+        <h1>About</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt quaerat exercitationem cum non, nisi commodi reiciendis. Tempore explicabo repudiandae, facere ad cupiditate recusandae perferendis eligendi nihil rem unde illum et!
+    </main>
+</template>
+
+<style scoped>
+    p{
+        text-align: center;
+    }
+</style>
---
+++ src/views/CarView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {useRoute} from 'vue-router'
+import {onBeforeMount, ref} from "vue"
+import cars from "../data.json"
+
+const car = ref(null)
+const route = useRoute()
+const {id} = route.params
+
+onBeforeMount(() => {
+    car.value = cars.find(c => c.id === parseInt(id))
+})
+
+
+</script>
+
+<template>
+    <div class="container">
+        <div v-if="car">
+            <h1>The Car</h1>
+             <p>Make: {{ car.make }}</p>
+             <p>Body: {{ car.body }}</p>
+             <p>Price: {{ car.price }}</p>
+             <p>year: {{ car.year }}</p>
+        </div>
+    
+    <div v-else> 
+    <h1>Car not found</h1>
+    </div>  
+    </div>
+</template>
